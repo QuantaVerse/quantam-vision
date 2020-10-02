e202f19fd15c2bd69c16e224c11962303cb17d54
Pritam <pritam.sarkar@medlife.com>
2020-10-03 03:21:57 +0530

    Add HighCharts library

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ChartSample from "./ChartSample";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ChartSample/>
     </div>
   );
 }
---
+++ src/ChartSample.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+const options: Highcharts.Options = {
+    title: {
+        text: 'My chart'
+    },
+    series: [{
+        type: 'line',
+        data: [1, 2, 3]
+    }]
+}
+
+function ChartSample(props: HighchartsReact.Props) {
+    return (
+        <div>h
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={options}
+                {...props}
+            />
+        </div>
+    );
+}
+
+export default ChartSample;
