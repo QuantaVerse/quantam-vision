adbd0bd871b642198d360a5730d4975e6fdd1243
Pritam <pritam.sarkar@medlife.com>
2020-10-10 06:27:05 +0530

    Sample amchart

--- src/components/ChartSample.tsx
+++ src/components/ChartSample.tsx
@@ -1,25 +1,7 @@
 import React from "react";
-import * as Highcharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
 
-const options: Highcharts.Options = {
-    title: {
-        text: "My chart"
-    },
-    series: [
-        {
-            type: "line",
-            data: [1, 2, 3]
-        }
-    ]
-};
-
-function ChartSample(props: HighchartsReact.Props) {
-    return (
-        <div>
-            <HighchartsReact highcharts={Highcharts} options={options} {...props} />
-        </div>
-    );
+function ChartSample() {
+    return <div></div>;
 }
 
 export default ChartSample;
--- src/pages/ProxyManager.tsx
+++ src/pages/ProxyManager.tsx
@@ -1,7 +1,122 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_dark from "@amcharts/amcharts4/themes/dark";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
-const ProxyManager = () => {
-    return <div className="proxy-manager">Hello</div>;
-};
+interface ProxyAPIStats {
+    status: string;
+    api_hit_rate: number | null;
+}
+
+interface IDataProxyConfig {
+    openExchanges: string[];
+    intraDayIntervals: string[];
+    additionalConfig: Record<string, string>;
+}
+
+interface DataProxyStats {
+    readonly name: string;
+    readonly api_key_name: string;
+    readonly proxy_config: IDataProxyConfig;
+    readonly api_stats: ProxyAPIStats;
+}
+
+type ProxyStatsLedger = Record<string, DataProxyStats>;
+
+interface ProxyManagerState {
+    proxy_manager_stats: ProxyStatsLedger | null;
+}
+
+class ProxyManager extends Component<{}, ProxyManagerState> {
+    state: Readonly<ProxyManagerState> = {
+        proxy_manager_stats: null
+    };
+
+    async componentDidMount() {
+        this.setState({ proxy_manager_stats: null });
+        await axios
+            .get<ProxyStatsLedger>("http://localhost:3000/api/v1/quantam_core/proxy_manager/stats")
+            .then((resp) => {
+                if (resp?.data) {
+                    this.setState({ proxy_manager_stats: resp.data });
+                } else {
+                    console.warn(resp);
+                }
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        am4core.useTheme(am4themes_dark);
+        // Themes end
+        // Create chart instance
+        let chart = am4core.create("chartdiv", am4charts.PieChart);
+
+        // Add data
+        chart.data = [
+            {
+                country: "Lithuania",
+                litres: 501.9
+            },
+            {
+                country: "Czech Republic",
+                litres: 301.9
+            },
+            {
+                country: "Ireland",
+                litres: 201.1
+            },
+            {
+                country: "Germany",
+                litres: 165.8
+            },
+            {
+                country: "Australia",
+                litres: 139.9
+            },
+            {
+                country: "Austria",
+                litres: 128.3
+            },
+            {
+                country: "UK",
+                litres: 99
+            },
+            {
+                country: "Belgium",
+                litres: 60
+            },
+            {
+                country: "The Netherlands",
+                litres: 50
+            }
+        ];
+
+        // Add and configure Series
+        let pieSeries = chart.series.push(new am4charts.PieSeries());
+        pieSeries.dataFields.value = "litres";
+        pieSeries.dataFields.category = "country";
+    }
+
+    render() {
+        const { proxy_manager_stats } = this.state;
+
+        let proxyHitRates: [string, number | null][] = [];
+        if (proxy_manager_stats !== null) {
+            Object.keys(proxy_manager_stats).forEach((proxyName) => {
+                proxyHitRates.push([proxyName, proxy_manager_stats[proxyName]?.api_stats?.api_hit_rate]);
+            });
+        }
+        return (
+            <div className="proxy-manager">
+                <div id="chartdiv" style={{ backgroundColor: "grey" }}></div>
+            </div>
+        );
+    }
+}
 
 export default ProxyManager;
